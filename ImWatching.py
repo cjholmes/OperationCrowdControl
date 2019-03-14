61cb87c0035f957f917363e9b4c57f3773086e6d
cjholmes <cjholmes@mtu.edu>
2019-03-14 12:30:59 -0400

    More Setup

--- ImWatching.py
+++ ImWatching.py
@@ -4,4 +4,24 @@ import pyaudio
 
 #TODO Figure out capture device of camera
 vCap = cv2.VideoCapture(0)
+
 # TODO Add audio capture
+pa = pyaudio.PyAudio()
+
+stream = pa.open(format = pyaudio.paFloat32,
+                 channels = 1,
+                 rate = 44100,
+                 input = True,
+                 frames_per_buffer = 1024,
+                 stream_callback = checkForActivate)
+
+while stream.is_active():
+    print "listening"
+
+
+
+def checkForActivate(in_data, # recorded data if input=True
+                     frame_count, # number of frames
+                     time_info, #dictionary
+                     status_flags): #pyaudio callback flags
+    print "Is Callback"
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # OperationCrowdControl
 Using anaconda2018.12 https://www.anaconda.com/distribution/
+
 Installed openCV using "conda install -c menpo opencv"
+
 Installed pyaudio using "conda install pyaudio"
